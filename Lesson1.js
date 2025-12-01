25b188fd97d6783c0c0900e8dd7bab060bf8f8e0
Marcio <marcio101167@gmail.com>
2025-12-01 14:25:33 +0200

    Enhancing my first js program

--- Lesson1.js
+++ Lesson1.js
@@ -1,2 +1,18 @@
 // 1. Hello World
 console.log("Hello, World!")
+
+//Variables
+var firstName = "John"
+let lastName = "Doe"
+console.log(firstName)
+
+var age, dateOfBirth, sex
+age = "30"
+sex = "Male"
+console.log(age)
+age = "6"
+console.log(age)
+
+//constants
+const occupation = "Engineer"
+console.log(occupation)
