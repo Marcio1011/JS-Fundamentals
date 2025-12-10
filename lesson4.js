78e6125e93e0e0569a11d1cf88087fbcf2ff6a45
Marcio <marcio101167@gmail.com>
2025-12-10 08:03:21 +0200

    Relational or Comparison Operators

---
+++ lesson4.js
@@ -0,0 +1,10 @@
+//Relational or Comparison Operators
+
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+// == equal to (value only)
+// === equal to (value and type)
+// != not equal to (value only)
+// !== not equal to (value and type)
