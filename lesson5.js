e6c17dada0f57bf02b6ce08e5422547d10625880
Marcio <marcio101167@gmail.com>
2025-12-10 08:47:49 +0200

    Logical operators

---
+++ lesson5.js
@@ -0,0 +1,17 @@
+// Logical "AND"
+//consule.log(true && true);   // all values have to be TRUE for the expression to be TRUE
+
+// Logical "OR"
+//consule.log(true || false);  // at least one value has to be TRUE for the expression to be TRUE
+
+var ageIsMoreThan18 = true
+var isUSCitizen = true
+
+var eligibleForDrivingLicense = ageIsMoreThan18 && isUSCitizen
+console.log('This customer is eligible for a driving license: ' + eligibleForDrivingLicense)
+
+//Logical "NOT"
+console.log(!true);  // negation, opposite of the value
+
+
+
