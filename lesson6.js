5d7a951ffc17d19ebc8cd22ac9da71827ac08263
Marcio <marcio101167@gmail.com>
2025-12-11 14:03:14 +0200

    Conditional statement

---
+++ lesson6.js
@@ -0,0 +1,14 @@
+//Conditional Statement
+
+//if hour is between 6 and 12 print "Good Morning"
+//if hour is between 12 and 18 print "Good Afternoon"
+//otherwise print "Good Evening"
+
+var hour = 5
+if (hour >= 6 && hour < 12) {
+    console.log("Good Morning")
+} else if (hour >= 12 && hour < 18) {
+    console.log("Good Afternoon")
+} else {
+    console.log("Good Evening")
+}
